a617bf765cfe140783bb99340367a92d4afd62ad
pascalDev <pascalyakouyami@gmail.com>
2024-05-29 10:09:04 +0100

    u

--- customer-service/src/main/resources/db/migration/V1__init_customer.sql
+++ customer-service/src/main/resources/db/migration/V1__init_customer.sql
@@ -1,12 +1,10 @@
-create sequence customer_seq start with 1 increment by 1
-
 create table customer (
         id bigint not null,
         email varchar(255),
         first_name varchar(255),
         last_name varchar(255),
         primary key (id)
-    )
-
+    );
+create sequence customer_seq start with 1 increment by 1;
 
 
