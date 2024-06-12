88d7da9f4d984267169b1c2746fda013820219eb
pascalDev <pascalyakouyami@gmail.com>
2024-06-12 09:43:34 +0100

    u

--- member-service/src/main/java/org/dev/controller/MemberController.java
+++ member-service/src/main/java/org/dev/controller/MemberController.java
@@ -21,15 +21,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
+@RequiredArgsConstructor
 public class MemberController {
 	
-	  @Autowired
-	 MemberServiceImpl memberService;
+	  
+	private final MemberServiceImpl memberService;
 	
 	@GetMapping("/members/{id}")
 	@ResponseStatus(HttpStatus.OK)
