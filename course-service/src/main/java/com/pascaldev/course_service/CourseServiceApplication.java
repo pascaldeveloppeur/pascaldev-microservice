d599403290fd905cfe8ad7b232b67be32caaa4fe
pascalDev <pascalyakouyami@gmail.com>
2024-06-14 11:23:38 +0100

    u

--- course-service/src/main/java/com/pascaldev/course_service/CourseServiceApplication.java
+++ course-service/src/main/java/com/pascaldev/course_service/CourseServiceApplication.java
@@ -2,12 +2,20 @@ package com.pascaldev.course_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition
+@Slf4j
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseServiceApplication.class, args);
+		log.info("Course service started!");
 	}
 
 }
---
+++ course-service/src/main/java/com/pascaldev/course_service/model/Course.java
@@ -0,0 +1,47 @@
+package com.pascaldev.course_service.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Pascal Dev
+ */
+@Entity
+@Table(name = "course")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course implements Serializable {
+	
+	
+	private static final long serialVersionUID = -3598149376138964228L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_seq")
+	@SequenceGenerator(name = "course_seq", sequenceName = "course_seq", initialValue = 1, allocationSize = 1)
+	private Long id;
+
+	@NotNull(message = "courseName cannot be null")
+	@Column(name = "courseName", length = 20)
+	private String courseName;
+	  
+	@OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+	private List<Subject> subjects;
+
+}
