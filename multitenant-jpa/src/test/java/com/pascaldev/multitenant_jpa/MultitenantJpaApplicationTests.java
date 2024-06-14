249e254712d0905fa425f8f20ece6d2bf188868a
pascalDev <pascalyakouyami@gmail.com>
2024-06-14 14:59:34 +0100

    u

---
+++ multitenant-jpa/src/test/java/com/pascaldev/multitenant_jpa/MultitenantJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascaldev.multitenant_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
