08bcbf31223471daa515d9aec4db03e139fba955
pascalDev <pascalyakouyami@gmail.com>
2024-06-20 14:39:55 +0100

    u

---
+++ pascaldev-utils-service/src/main/java/com/pascaldev/pascaldev_utild_service/PascaldevUtildServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pascaldev.pascaldev_utild_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PascaldevUtildServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PascaldevUtildServiceApplication.class, args);
+	}
+
+}
---
+++ pascaldev-utils-service/src/test/java/com/pascaldev/pascaldev_utild_service/PascaldevUtildServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascaldev.pascaldev_utild_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PascaldevUtildServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
