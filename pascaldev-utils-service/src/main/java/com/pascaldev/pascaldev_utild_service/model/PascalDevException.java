cb86bc4960ff13ce1995263e376e55d1aa379f40
pascalDev <pascalyakouyami@gmail.com>
2024-06-20 16:33:06 +0100

    u

---
+++ pascaldev-utils-service/src/main/java/com/pascaldev/pascaldev_utild_service/model/PascalDevException.java
@@ -0,0 +1,50 @@
+package com.pascaldev.pascaldev_utild_service.model;
+
+import org.slf4j.helpers.MessageFormatter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author Pascal Dev
+ */
+@SuppressWarnings("serial")
+public class PascalDevException extends RuntimeException {
+
+	
+	private int statusCode;
+
+	public PascalDevException() {
+		
+		this.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	}
+	
+    public PascalDevException(String message) {
+		
+		this(HttpStatus.INTERNAL_SERVER_ERROR.value(), message);
+	}
+    
+    public PascalDevException(String message, Object... args) {
+		
+		this(HttpStatus.INTERNAL_SERVER_ERROR.value(), MessageFormatter.arrayFormat(message, args).getMessage());
+	}
+	
+	
+
+	public PascalDevException(int statutCode, String message) {
+		super(message);
+		this.setStatusCode(statutCode);
+	}
+	
+	
+
+	
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+		
+	}
+
+}
