44028f2dc6068bc3aa3936a75c129d4416178b1e
pascalDev <pascalyakouyami@gmail.com>
2024-06-20 19:26:48 +0100

    u

--- pascaldev-registrion-service/src/main/java/com/pascaldev/registrionService/RegistrionServiceApplication.java
+++ pascaldev-registrion-service/src/main/java/com/pascaldev/registrionService/RegistrionServiceApplication.java
@@ -2,10 +2,12 @@ package com.pascaldev.registrionService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Slf4j
 public class RegistrionServiceApplication {
 
