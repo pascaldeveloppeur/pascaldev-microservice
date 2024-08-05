d65cc2e0a31cc233a904e327f214bef244f6a179
pascalDev <pascalyakouyami@gmail.com>
2024-08-05 12:51:07 +0100

    u

--- pascaldev-user-service/src/main/java/com/pascaldev/user_service/repository/UserRepository.java
+++ pascaldev-user-service/src/main/java/com/pascaldev/user_service/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.pascaldev.user_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pascaldev.user_service.model.User;
+import java.util.List;
+
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	
 }
--- pascaldev-user-service/src/main/java/com/pascaldev/user_service/service/UserService.java
+++ pascaldev-user-service/src/main/java/com/pascaldev/user_service/service/UserService.java
@@ -3,6 +3,7 @@ package com.pascaldev.user_service.service;
 import com.pascaldev.pascaldev_utild_service.service.AbstractService;
 import com.pascaldev.user_service.dto.UserDto;
 
+@SuppressWarnings("hiding")
 public interface UserService<UserDto> extends AbstractService<UserDto> {
 
 }
